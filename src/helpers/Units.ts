e196c9d16ddb352301f1a15225eeec39b4315a7c
Christopher Atwood <atwoodc@cardiff.ac.uk>
2021-10-09 22:04:51 +0100

    Documented and united tested units helper

--- src/helpers/Units.ts
+++ src/helpers/Units.ts
@@ -10,27 +10,56 @@ export enum SPLIT {
 export enum UNITS {
   'd1000' = 'KMs',
   'd400' = 'laps',
-  'd1609.34' = 'm',
+  'd1609.34' = 'miles',
 }
 
 const hour = 3600;
 
+/**
+ * Converts meters to KMs
+ * @param {number} meters Meters
+ * @returns  {number} distance as KMs
+ */
 export function toKM(meters: number): number {
   return meters / SPLIT.KM;
 }
-
+/**
+ * Format distance as string with unit
+ *
+ * @param {number} meters Total meters
+ * @param {number} split User current split choice
+ * @returns  {string} Formatted distance with unit
+ */
 export function formatDistance(meters: number, split: SPLIT): string {
   return `${(meters / split).toFixed(2)} ${UNITS[`d${split}` as 'd1000' | 'd400' | 'd1609.34']}`;
 }
-
+/**
+ * Format distance as number
+ *
+ * @param {number} meters Total meters
+ * @param {number} split User current split choice
+ * @returns {number}
+ */
 export function formatDistanceAsNumber(meters: number, split: SPLIT): number {
   return meters / split;
 }
 
+/**
+ * Converts KMs to meters
+ * @param {number} KMs KMs
+ * @returns {number} Distance in meters
+ */
 export function toMeters(KMs: number): number {
-  return KMs * 1000;
+  return KMs * SPLIT.KM;
 }
 
+/**
+ * Format time used for pacing
+ *
+ * @param {Duration} duration duration
+ * @param {boolean} full Whether to include leading 0s
+ * @returns {string} String formatted time
+ */
 export function formatTime(duration?: Duration, full = false): string {
   if (duration === undefined) return '';
 
@@ -41,22 +70,53 @@ export function formatTime(duration?: Duration, full = false): string {
   return full ? duration.toFormat('mm:ss') : duration.toFormat('m:ss');
 }
 
+/**
+ * Format a datetime to DATETIME_FULL
+ *
+ * @param {DateTime} datetime DateTime to format
+ * @returns {string} Formatted string
+ */
 export function formatDateTime(datetime: DateTime): string {
   return datetime.toLocaleString(DateTime.DATETIME_FULL);
 }
 
+/**
+ * Convert meters per KM to meters per minute
+ * @param {number} meters Total meters per hour
+ * @returns {number} Total meters per minute
+ */
 export function metersToKmPerMinute(meters: number): number {
   return meters / 16.6667;
 }
-
+/**
+ * Converts a number to degree
+ *
+ * @param number Number
+ * @returns {number} number in degrees
+ */
 export function toDegree(number: number): number {
   return (number * 180) / Math.PI;
 }
+
+/**
+ * Converts a number to radian
+ *
+ * @param number Number
+ * @returns {number} number in radian
+ */
 export function toRadian(number: number): number {
   return (number * Math.PI) / 180;
 }
 
+/**
+ * Returns percentage of a number
+ *
+ * @param {number} percentage Percentage
+ * @param {number} total  Total value
+ * @returns {number}
+ */
 export function percentageOf(percentage: number, total: number): number {
   return (percentage / 100) * total;
 }
+
 export default {};
--- tests/unit/helpers/unit.spec.ts
+++ tests/unit/helpers/unit.spec.ts
@@ -1,11 +1,47 @@
-import { Duration } from 'luxon';
+import { DateTime, Duration } from 'luxon';
 import {
-  formatTime, formatDistance, metersToKmPerMinute, SPLIT,
+  formatTime,
+  formatDistance,
+  metersToKmPerMinute,
+  SPLIT,
+  toKM,
+  formatDistanceAsNumber,
+  toMeters,
+  formatDateTime,
+  percentageOf,
+  toDegree,
+  toRadian,
 } from '@/helpers/Units';
 
 describe('Unit helper', () => {
-  test('Meters to KMs', () => {
-    expect(formatDistance(1, SPLIT.KM)).toBe(0.001);
+  test('Meters to km (number)', () => {
+    expect(toKM(SPLIT.KM)).toBe(1.0);
+  });
+  describe('Format display based on split (string)', () => {
+    test('Meters to KMs', () => {
+      expect(formatDistance(SPLIT.KM, SPLIT.KM)).toBe('1.00 KMs');
+    });
+    test('Meters to Miles', () => {
+      expect(formatDistance(SPLIT.MILE, SPLIT.MILE)).toBe('1.00 miles');
+    });
+    test('Meters to laps', () => {
+      expect(formatDistance(SPLIT.LAP, SPLIT.LAP)).toBe('1.00 laps');
+    });
+  });
+  describe('convert distance to split unit (number)', () => {
+    test('Meters to KMs', () => {
+      expect(formatDistanceAsNumber(SPLIT.KM, SPLIT.KM)).toBe(1.0);
+    });
+    test('Meters to Miles', () => {
+      expect(formatDistanceAsNumber(SPLIT.MILE, SPLIT.MILE)).toBe(1.0);
+    });
+    test('Meters to laps', () => {
+      expect(formatDistanceAsNumber(SPLIT.LAP, SPLIT.LAP)).toBe(1.0);
+    });
+  });
+
+  test('KMs to meters', async () => {
+    expect(toMeters(1)).toBe(1000);
   });
 
   describe('Format time', () => {
@@ -21,7 +57,22 @@ describe('Unit helper', () => {
       expect(formatTime(Duration.fromObject({ hours: 1, seconds: 1 }), true)).toBe('01:00:01');
     });
   });
+
+  test('Format datetime', () => {
+    const currentDateTime = DateTime.fromISO('2021-10-09T20:53:39+0000');
+
+    expect(formatDateTime(currentDateTime)).toBe('9 October 2021, 21:53 BST');
+  });
   test('Meters to KM per min', () => {
     expect(metersToKmPerMinute(1)).toBe(0.05999988000024);
+  });
+  test('number to degree', () => {
+    expect(toDegree(1)).toBe(57.29577951308232);
+  });
+  test('number to radian', () => {
+    expect(toRadian(10)).toBe(0.17453292519943295);
+  });
+  test('percentage of a number', () => {
+    expect(percentageOf(50, 4)).toBe(2);
   });
 });
